92cfe333188dd11b0a1347b99e61934020b83dbc
DESKTOP-JMGR3K9\wrtop <wrtoppass@gmail.com>
2023-05-25 09:27:23 -0500

    first commit

---
+++ w8_d2_hw.ts
@@ -0,0 +1,106 @@
+interface Attackable{
+    attack():void
+}
+
+interface Defendable{
+    defend():void
+}
+
+abstract class Character implements Attackable, Defendable{
+    protected gold$:number = 0
+    protected attackAbility$:Attackable | undefined
+    protected defenseAbility$:Defendable | undefined
+
+
+    set attackAbility(value:Attackable){
+        this.attackAbility$=value
+    }
+    set defenseAbility(value:Defendable){
+        this.defenseAbility$=value
+    }
+    set gold(value:number){
+        this.gold$=value
+    }
+    get gold(){
+        return this.gold$
+    }
+    collectGold(value:number):void{
+        this.gold$+=value
+    }
+    attack(){
+        if(this.attackAbility$){
+            this.attackAbility$.attack()
+        }
+    }
+    defend(){
+        if(this.defenseAbility$){
+            this.defenseAbility$.defend()
+        }
+    } 
+}
+
+class Bow implements Attackable{
+    attack(){
+        console.log("Shoots a bow and arrow")
+    }
+}
+
+class Club implements Attackable{
+    attack(){
+        console.log('Bludgeons with a club')
+    }
+}
+class Sword implements Attackable{
+    attack(){
+        console.log('Swings a sword')
+    }
+}
+class Armor implements Defendable{
+    defend(){
+        console.log('Armor is taking damage')
+    }
+}
+class Shield implements Defendable{
+    defend(){
+        console.log('Shield is taking damage')
+    }
+}
+class Tunic implements Defendable{
+    defend(){
+        console.log('Tunic is taking damage')
+    }
+}
+
+class Ogre extends Character{
+    attackAbility$=new Club()
+    defenseAbility$=new Shield()
+}
+class Peon extends Character{
+    attackAbility$=new Club()
+    defenseAbility$=new Shield()
+}
+class Knight extends Character{
+    attackAbility$=new Sword()
+    defenseAbility$=new Armor()
+}
+class Archer extends Character{
+    attackAbility$=new Bow()
+    defenseAbility$=new Tunic()
+}
+
+let bigboi=new Ogre()
+bigboi.attack()
+bigboi.defend()
+
+let paladin=new Ogre()
+paladin.attack()
+paladin.defend()
+
+let serf=new Ogre()
+serf.attack()
+serf.defend()
+
+let forestelf=new Ogre()
+forestelf.attack()
+forestelf.defend()
+
